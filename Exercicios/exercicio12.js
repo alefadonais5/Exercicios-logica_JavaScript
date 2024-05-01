0dccbe27214e85909446f0b2c4dc42cd1daac554
alefadonais5 <alefadonais5@gmail.com>
2024-05-01 20:57:25 -0300

    Exercícios de Lógica de Programação em JavaScript

--- Exercicios/exercicio12.js
+++ Exercicios/exercicio12.js
@@ -1,6 +1,11 @@
 let Total_Eleitores = parseInt(prompt("Quantos eleitores tem no municipio "))
-let Votos_Valido = parseInt(prompt("Quantos votos foram a validos? "))
+let Votos_Validos = parseInt(prompt("Quantos votos foram a validos? "))
 let Votos_Branco = parseInt(prompt("Quantos votos em branco? "))
 let Votos_Nulo = parseInt(prompt("Quantos votos nulos? "))
 
 //Falta fazer os calculos
+let Percentual_Validos = (Total_Eleitores*Votos_Valido)/100
+let Percentual_Branco = (Total_Eleitores*Votos_Branco)/100
+let Percentual_Nulo = (Total_Eleitores*Votos_Nulo)/100
+
+alert("Total de eleitores: " + Total_Eleitores + "/n Votos Válidos: " + Percentual_Validos + "/n Votos Brancos : " + Percentual_Branco + "/n Votos Nulos: " + Percentual_Nulo)
