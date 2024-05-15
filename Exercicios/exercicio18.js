f547422934622b7ed9486aa417687035d245eccf
alefadonais5 <alefadonais5@gmail.com>
2024-05-15 13:27:04 -0300

    Exercícios de Lógica de Programação em JavaScript

--- Exercicios/exercicio18.js
+++ Exercicios/exercicio18.js
@@ -1,12 +1,13 @@
-//falta concluir
 
 const array = [2,5,4,2,8,5,2];
 const novo_array = [];
 
-for (let x = 6; x <=0; x--){
-    novo_array=novo_array.push(array[x])
+for(let x = 0; x <= 6; x++){
+    const novo = novo_array.unshift(array[x])
 }
 
+console.log(array)
 console.log(novo_array)
+
 
 
